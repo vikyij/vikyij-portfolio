7e4c08f4796fc02d0eaa1a6be772081447842126
Victoria <vikkyij54@gmail.com>
2020-05-06 12:09:58 +0100

    final changes for now, hopefully

--- src/About.js
+++ src/About.js
@@ -16,7 +16,9 @@ const About = ({title, id}) => {
           <div className="center">
             <h1 className='logo'> < MdPhonelink /></h1>
             <h2 className='about-details2'>Responsive</h2>
-            <p className='about-details'>Fast load times and lag free interaction, my highest priority.</p>
+            <p className='about-details'>
+            Websites and pages that automatically adapts to all screen, whether it's a desktop, laptop, tablet, or smartphone. 
+            </p>
           </div>
         </div>
 
@@ -24,23 +26,23 @@ const About = ({title, id}) => {
           <div className="center">
             <h1 className='logo'> < IoIosFlash /></h1>
             <h2 className='about-details2'>Fast</h2>
-            <p className='about-details'>Fast load times and lag free interaction, my highest priority.</p>
+            <p className='about-details'>Websites load with high speed and are lag free.</p>
           </div>
         </div>
 
         <div className='col-6 col-md-3'>
           <div className="center">
             <h1 className='logo'> < MdPhonelink /></h1>
-            <h2 className='about-details2'>Responsive</h2>
-            <p className='about-details'>Fast load times and lag free interaction, my highest priority.</p>
+            <h2 className='about-details2'>Effective Navigation</h2>
+            <p className='about-details'>Navigating through pages is easy,clutter-free and the navigation appears consistent on all browsers and platforms.</p>
           </div>
         </div>
 
         <div className='col-6 col-md-3'>
           <div className="center">
             <h1 className='logo'> < MdPhonelink /></h1>
-            <h2 className='about-details2'>Responsive</h2>
-            <p className='about-details'>Fast load times and lag free interaction, my highest priority.</p>
+            <h2 className='about-details2'>User Friendly</h2>
+            <p className='about-details'>User friendly websites that are simple,easy to use and built with users in mind</p>
           </div>
         </div>
       </div>
@@ -49,10 +51,11 @@ const About = ({title, id}) => {
         <div className='row'>
           <div className='col-sm-6 col-md-5 my-img'>
             <img src={imagesvg} className='image-logo' alt="logo" />
-            <h2 className='about-details2'>Who's this guy?</h2>
-            <p className='about-details'>I'm a Front-End Developer for ChowNow in Los Angeles, CA.
-            I have serious passion for UI effects, animations and creating intuitive, dynamic user experiences.
-Let's make something special.</p>
+            <h2 className='about-details2'>Who's this girl?</h2>
+            <p className='about-details'>I'm a Front-End Developer.
+            I have three years of hands-on experience efficiently coding websites and applications using modern HTML, CSS, and JavaScript.
+            I have passion for creating beautiful,easy to use, user-friendly websites and applications.
+           </p>
           </div>
 
           <div className='col-sm-6 col-md-7 statistics'>
