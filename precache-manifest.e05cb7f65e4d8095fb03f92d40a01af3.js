fe118467c9a164135a5674c09f91e931e0d28b31
Victoria <vikkyij54@gmail.com>
2020-05-06 13:40:53 +0100

    Updates

---
+++ precache-manifest.e05cb7f65e4d8095fb03f92d40a01af3.js
@@ -0,0 +1,66 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6645b4bbfb013d338bac72af33b0d0d1",
+    "url": "/vikyij-portfolio/index.html"
+  },
+  {
+    "revision": "05f6cf5dd383d5df40fb",
+    "url": "/vikyij-portfolio/static/css/2.74189dbb.chunk.css"
+  },
+  {
+    "revision": "f21230e0fd86b1296474",
+    "url": "/vikyij-portfolio/static/css/main.43701634.chunk.css"
+  },
+  {
+    "revision": "05f6cf5dd383d5df40fb",
+    "url": "/vikyij-portfolio/static/js/2.c4586522.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/vikyij-portfolio/static/js/2.c4586522.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f21230e0fd86b1296474",
+    "url": "/vikyij-portfolio/static/js/main.86c0f9db.chunk.js"
+  },
+  {
+    "revision": "43c5c04ad8f14b27770d",
+    "url": "/vikyij-portfolio/static/js/runtime-main.de9eb6b8.js"
+  },
+  {
+    "revision": "4bf2f124452a69cc104ae3fa7832cad0",
+    "url": "/vikyij-portfolio/static/media/s1.4bf2f124.PNG"
+  },
+  {
+    "revision": "9de4be5034e58f6f2ab5ce448761f6a8",
+    "url": "/vikyij-portfolio/static/media/s2.9de4be50.PNG"
+  },
+  {
+    "revision": "8e53982a5d91e0cc18be0d18c1d0507c",
+    "url": "/vikyij-portfolio/static/media/s3.8e53982a.PNG"
+  },
+  {
+    "revision": "0065e40f666efe2e7ff5ae9ebb332998",
+    "url": "/vikyij-portfolio/static/media/s4.0065e40f.PNG"
+  },
+  {
+    "revision": "4c2920a2c899cfea7176e8a957c26ce0",
+    "url": "/vikyij-portfolio/static/media/s5.4c2920a2.PNG"
+  },
+  {
+    "revision": "58ef65f05e0bf9d42e333cda74ee60d7",
+    "url": "/vikyij-portfolio/static/media/s6.58ef65f0.PNG"
+  },
+  {
+    "revision": "b55278f26296e2ae4997ab63d963b7f1",
+    "url": "/vikyij-portfolio/static/media/undraw_feedback_h2ft.b55278f2.svg"
+  },
+  {
+    "revision": "252b2ca2904e2e70078a5fbedbc48cf6",
+    "url": "/vikyij-portfolio/static/media/undraw_female_avatar_w3jk.252b2ca2.svg"
+  },
+  {
+    "revision": "1941636394b7ffca180bc1a1d7d4ef2c",
+    "url": "/vikyij-portfolio/static/media/undraw_react_y7wq.19416363.svg"
+  }
+]);
